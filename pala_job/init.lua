483c65d89da35ccb2c25dc0da23ba2c3f8fbbcfd
AFCMS <afcm.contact@gmail.com>
2020-12-18 18:20:01 +0100

    jobs system fixes - levels, inventory

--- pala_job/init.lua
+++ pala_job/init.lua
@@ -64,16 +64,47 @@ end
 
 function pala_job.earn_xp(player, job, list)
 	local meta = player:get_meta()
-	--minetest.chat_send_all(tostring(meta:get_float("pala_job_xp_"..job)))
-	meta:set_float("pala_job_xp_"..job, meta:get_float("pala_job_xp_"..job) + list[pala_job.get_level(player, job)+1])
-	--pala_job.update_job
-	minetest.chat_send_all(tostring(player:get_meta():get_float("pala_job_xp_"..job)))
-	if meta:get_float("pala_job_xp_"..job) >= pala_job.levels[meta:get_int("pala_job_level_"..job) + 1] then
-		local oldlevel = player:get_meta():get_int("pala_job_level_"..job)
-		player:get_meta():set_int("pala_job_level_"..job, player:get_meta():get_int("pala_job_level_"..job) + 1)
-		minetest.chat_send_all("lvl passed")
-		pala_job.give_loot(player, job, player:get_meta():get_int("pala_job_level_"..job))
+	if pala_job.levels[meta:get_int("pala_job_level_"..job) + 1] then
+		--minetest.chat_send_all(tostring(meta:get_float("pala_job_xp_"..job)))
+		meta:set_float("pala_job_xp_"..job, meta:get_float("pala_job_xp_"..job) + list[pala_job.get_level(player, job)+1])
+		--pala_job.update_job
+		minetest.chat_send_all(tostring(player:get_meta():get_float("pala_job_xp_"..job)))
+		if meta:get_float("pala_job_xp_"..job) >= pala_job.levels[meta:get_int("pala_job_level_"..job) + 1] then
+			local oldlevel = player:get_meta():get_int("pala_job_level_"..job)
+			player:get_meta():set_int("pala_job_level_"..job, player:get_meta():get_int("pala_job_level_"..job) + 1)
+			minetest.chat_send_all("lvl passed")
+			pala_job.give_loot(player, job, player:get_meta():get_int("pala_job_level_"..job))
+		end
 	end
+end
+
+function pala_job.reset_xp(player, job)
+	pala_job.set_xp(player, job, 0)
+	pala_job.update_job(player, job)
+end
+
+
+function pala_job.update_job(player, job)
+	local meta = player:get_meta()
+	if pala_job.levels[meta:get_int("pala_job_level_"..job) + 1] then
+		if meta:get_float("pala_job_xp_"..job) >= pala_job.levels[meta:get_int("pala_job_level_"..job) + 1] then
+			local oldlevel = player:get_meta():get_int("pala_job_level_"..job)
+			meta:set_int("pala_job_level_"..job, meta:get_int("pala_job_level_"..job) + 1)
+		end
+	end
+end
+
+
+function pala_job.set_xp(player, job, amount)
+	local meta = player:get_meta()
+	local l = pala_job.levels[20]
+	--if not amount>l then
+	meta:set_float("pala_job_xp_"..job, amount)
+	pala_job.update_job(player, job)
+		--return true
+	--else 
+		--return false
+	--end
 end
 
 --RAW:formspec_version[3]size[9,5.5]label[3.5,0.5;FARMER]label[3,2;You pass level 19]label[3,3.5;You receive :]image[1,4;1,1;]image[4,4;1,1;]image[7,4;1,1;]image[0,2.25;9,1;]
@@ -82,14 +113,16 @@ function pala_job.show_win_level(player, job, level, loot)
 	local loot2 = loot[level][2] or "mcl_core:stone"
 	local loot3 = loot[level][3] or "mcl_core:stone"
 	local form = table.concat({
+		"formspec_version[3]",
 		"size[9,5.5]",
+		"real_coordinates[true]",
 		"label[3.5,0.5;"..string.upper(job).."]",
 		"label[3,2;You pass level "..level.."]",
 		"label[3,3.5;You receive :]",
-		"item_image[1,4;1,1;1.0,1.0;"..loot1.."]",
-		"item_image[4,4;1,1;1.0,1.0;"..loot2.."]",
-		"item_image[7,4;1,1;1.0,1.0;"..loot3.."]",
-		"image[0,2.25;9,1;9.0,1.0;pala_job_level_up_border.png]"
+		"item_image[1,4;1,1;"..loot1.."]",
+		"item_image[4,4;1,1;"..loot2.."]",
+		"item_image[7,4;1,1;"..loot3.."]",
+		"image[0,2.25;9,1;pala_job_level_up_border.png]"
 	})
 	minetest.show_formspec(player:get_player_name(), "pala_job:level_up", form)
 end
@@ -125,6 +158,15 @@ minetest.register_chatcommand("reset", {
 		player:get_meta():set_int("pala_job_level_miner", 0)
 		player:get_meta():set_int("pala_job_xp_miner", 0)
 		return true, "Text was sent successfully"
+	end,
+})
+
+minetest.register_chatcommand("add_xp", {
+	description = "Send text to chat",
+	privs = {},
+	func = function(name, param)
+		local player = minetest.get_player_by_name(name)
+		return pala_job.set_xp(player, "miner", 598000)
 	end,
 })
 
