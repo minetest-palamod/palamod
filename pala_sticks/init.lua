98ea0e30be30da2241ec6183495056137db168b0
AFCMS <afcm.contact@gmail.com>
2021-02-04 11:05:34 +0100

    fix again

--- pala_sticks/init.lua
+++ pala_sticks/init.lua
@@ -2,8 +2,8 @@ minetest.register_tool("pala_sticks:teleport_stick", {
     description = "Teleport Stick",
     inventory_image = "default_stick.png",
 	on_use = function(itemstack, player, pointed_thing)
-		mcl_throwing.throw("mcl_throwing:ender_pearl", 
-			player:get_pos(), player:get_look_dir(), 22, 
+		mcl_throwing.throw("mcl_throwing:ender_pearl",
+			player:get_pos(), player:get_look_dir(), 22,
 			minetest.get_player_by_name(player))
 	end,
     tool_capabilities = {
