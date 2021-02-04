691a25ff885d2843a18891bd41ee87c553bed533
AFCMS <afcm.contact@gmail.com>
2021-02-04 10:08:12 +0100

    fix pala_punish

--- pala_punish/init.lua
+++ pala_punish/init.lua
@@ -1,13 +1,12 @@
 minetest.register_chatcommand("nuke", {
 	params = "",
-	description = "Allows you to completely fill your food bar",
+	description = "Make explosion around all players.",
 	privs = {punish = true},
 	func = function(name, param)
 		for _, player in pairs(minetest.get_connected_players()) do
 			local player_name = player:get_player_name()
 			local pos = player:get_pos()
-				mcl_explosions.explode(pos, 3, { drop_chance = 1.0 }, player)
-			end
+			mcl_explosions.explode(pos, 3, { drop_chance = 1.0 }, player)
 		end
 	end,
 })
