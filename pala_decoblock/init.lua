bd70d2878b87f4f7fa32ab9d724829ba93adbeca
AFCMS <61794590+AFCMS@users.noreply.github.com>
2021-02-04 18:49:37 +0100

    fix pala_decoblock

--- pala_decoblock/init.lua
+++ pala_decoblock/init.lua
@@ -18,9 +18,9 @@ if minetest.get_modpath("pala_paladium") then
 			corner_stair_texture_override)]]
 
 		mcl_stairs.register_stair("paladium", "pala_paladium:paladiumblock",
-			{pickaxey=4, building_block=1}, 
+			{pickaxey=4, building_block=1},
 			{"pala_paladium_paladium_block.png"},
-			("Paladium Stairs"), 
+			("Paladium Stairs"),
 			mcl_sounds.node_sound_stone_defaults(), 5, 5, --  <---- blast_resistance added
 			"woodlike")
 
@@ -45,25 +45,27 @@ if minetest.get_modpath("pala_paladium") then
 			("Amethyst Slab"),
 			mcl_sounds.node_sound_stone_defaults(), 5, 5, --  <---- blast_resistance added
 			("Double Amethyst Slab"))
+
 		mcl_stairs.register_stair("amethyst", "pala_paladium:amethystblock",
 			{pickaxey=4, building_block=1},
 			{"pala_paladium_amethyst_block.png"},
 			("Amethyst Stairs")
-			, mcl_sounds.node_sound_stone_defaults(), 5, 5, 
+			, mcl_sounds.node_sound_stone_defaults(), 5, 5,
 			"woodlike")
 
 		--Findium
-		mcl_stairs.register_slab("findium", "pala_paladium:findiumblock", 
+		mcl_stairs.register_slab("findium", "pala_paladium:findiumblock",
 			{pickaxey=4, building_block=1},
-			{"pala_paladium_findiumblock.png"}, 
-			("Findium Slab"), 
-			mcl_sounds.node_sound_stone_defaults(), 5, 5, 
+			{"pala_paladium_findiumblock.png"},
+			("Findium Slab"),
+			mcl_sounds.node_sound_stone_defaults(), 5, 5,
 			("Double Findium Slab"))
-		mcl_stairs.register_stair("findium", "pala_paladium:findiumblock", 
-			{pickaxey=4, building_block=1}, 
-			{"pala_paladium_findiumblock.png"}, 
-			("Findium Stairs"), 
-			mcl_sounds.node_sound_stone_defaults(), 5, 5, 
+
+		mcl_stairs.register_stair("findium", "pala_paladium:findiumblock",
+			{pickaxey=4, building_block=1},
+			{"pala_paladium_findiumblock.png"},
+			("Findium Stairs"),
+			mcl_sounds.node_sound_stone_defaults(), 5, 5,
 			"woodlike")
 	end
 end
