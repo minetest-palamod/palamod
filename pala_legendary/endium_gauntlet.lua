ce8253a4ce1b57014ddb41971cdf9e998edfcee9
AFCMS <afcm.contact@gmail.com>
2021-03-06 18:42:59 +0100

    pala_legendary endium gauntlet fixes (colors and usage)

--- pala_legendary/endium_gauntlet.lua
+++ pala_legendary/endium_gauntlet.lua
@@ -1,3 +1,6 @@
+local C = minetest.colorize
+local S = minetest.get_translator(minetest.get_current_modname())
+
 minetest.register_craftitem("pala_legendary:endium_gauntlet", {
 	description = ("Endium Gauntlet"),
 	_doc_items_longdesc = ("Just right-click to use it."),
@@ -5,5 +8,8 @@ minetest.register_craftitem("pala_legendary:endium_gauntlet", {
 	stack_max = 1,
 	groups = {},
 	on_use = function(itemstack, player, pointed_thing)
+	end,
+	on_place = function(itemstack, player, pointed_thing)
+		minetest.chat_send_player(player:get_player_name(), S(C(mcl_colors.RED, "There is no stone in the gauntlet.")))
 	end,
 })
