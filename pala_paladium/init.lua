a96bb334c089e422b460a56314eb136c4ab6d3c9
AFCMS <afcm.contact@gmail.com>
2021-04-12 02:18:40 +0200

    add translation to pala_paladium

--- pala_paladium/init.lua
+++ pala_paladium/init.lua
@@ -1,10 +1,12 @@
 local load_time = os.clock()
 minetest.log("action", "[pala_paladium] loading...")
 
+local S = minetest.get_translator(minetest.get_current_modname())
+
 --Amethyst
 minetest.register_node("pala_paladium:stone_with_amethyst", {
-	description = ("Amethyst Ore"),
-	_doc_items_longdesc = ("Amethyst ore is rare and can be found in clusters near the bottom of the world."),
+	description = S("Amethyst Ore"),
+	_doc_items_longdesc = S("Amethyst ore is rare and can be found in clusters near the bottom of the world."),
 	tiles = {"default_stone.png^pala_paladium_mineral_amethyst.png"},
 	is_ground_content = true,
 	stack_max = 64,
@@ -15,16 +17,16 @@ minetest.register_node("pala_paladium:stone_with_amethyst", {
 })
 
 minetest.register_craftitem("pala_paladium:amethyst_ingot", {
-	description = ("Amethyst Ingot"),
-	_doc_items_longdesc = ("Molten Amethyst. It is used to craft armor, tools, and whatnot."),
+	description = S("Amethyst Ingot"),
+	_doc_items_longdesc = S("Molten Amethyst. It is used to craft armor, tools, and whatnot."),
 	inventory_image = "default_steel_ingot.png^[colorize:#944dc7:150",
 	stack_max = 64,
 	groups = { craftitem=1 },
 })
 
 minetest.register_node("pala_paladium:amethystblock", {
-	description = ("Block of Amethyst"),
-	_doc_items_longdesc = ("A block of Amethyst is mostly a shiny decorative block."),
+	description = S("Block of Amethyst"),
+	_doc_items_longdesc = S("A block of Amethyst is mostly a shiny decorative block."),
 	tiles = {"pala_paladium_amethyst_block.png"},
 	is_ground_content = false,
 	stack_max = 64,
@@ -59,8 +61,8 @@ minetest.register_craft({
 
 --Titanium
 minetest.register_node("pala_paladium:stone_with_titanium", {
-	description = ("Titanium Ore"),
-	_doc_items_longdesc = ("Titanium ore is rare and can be found in clusters near the bottom of the world."),
+	description = S("@1 Ore", S("Titanium")),
+	_doc_items_longdesc = S("@1 ore is rare and can be found in clusters near the bottom of the world.", S("Titanium")),
 	tiles = {"default_stone.png^pala_paladium_mineral_titanium.png"},
 	is_ground_content = true,
 	stack_max = 64,
@@ -71,16 +73,16 @@ minetest.register_node("pala_paladium:stone_with_titanium", {
 })
 
 minetest.register_craftitem("pala_paladium:titanium_ingot", {
-	description = ("Titanium Ingot"),
-	_doc_items_longdesc = ("Molten Titanium. It is used to craft armor, tools, and whatnot."),
+	description = S("@1 Ingot", S("Titanium")),
+	_doc_items_longdesc = S("Molten @1. It is used to craft armor, tools, and whatnot.", S("Titanium")),
 	inventory_image = "pala_paladium_titanium_ingot.png",
 	stack_max = 64,
 	groups = { craftitem=1 },
 })
 
 minetest.register_node("pala_paladium:titaniumblock", {
-	description = ("Block of Titanium"),
-	_doc_items_longdesc = ("A block of Titanium is mostly a shiny decorative block."),
+	description = S("Block of @1", S("Titanium")),
+	_doc_items_longdesc = S("A block of @1 is mostly a shiny decorative block.", S("Titanium")),
 	tiles = {"pala_paladium_titanium_block.png"},
 	is_ground_content = false,
 	stack_max = 64,
@@ -115,8 +117,8 @@ minetest.register_craft({
 
 --Paladium
 minetest.register_node("pala_paladium:stone_with_paladium", {
-	description = ("Paladium Ore"),
-	_doc_items_longdesc = ("Paladium ore is rare and can be found in clusters near the bottom of the world."),
+	description = S("@1 Ore", S("Paladium")),
+	_doc_items_longdesc = S("@1 ore is rare and can be found in clusters near the bottom of the world.", S("Paladium")),
 	tiles = {"pala_paladium_mineral_paladium.png"},
 	is_ground_content = true,
 	stack_max = 64,
@@ -133,16 +135,16 @@ minetest.register_node("pala_paladium:stone_with_paladium", {
 })
 
 minetest.register_craftitem("pala_paladium:paladium_ingot", {
-	description = ("Paladium Ingot"),
-	_doc_items_longdesc = ("Molten Paladium. It is used to craft armor, tools, and whatnot."),
+	description = S("@1 Ingot", S("Paladium")),
+	_doc_items_longdesc = S("Molten @1. It is used to craft armor, tools, and whatnot.", S("Paladium")),
 	inventory_image = "pala_paladium_paladium_ingot.png",
 	stack_max = 64,
 	groups = { craftitem=1 },
 })
 
 minetest.register_node("pala_paladium:paladiumblock", {
-	description = ("Block of Paladium"),
-	_doc_items_longdesc = ("A block of Paladium is mostly a shiny decorative block."),
+	description = S("Block of @1", S("Paladium")),
+	_doc_items_longdesc = S("A block of @1 is mostly a shiny decorative block.", S("Paladium")),
 	tiles = {"pala_paladium_paladium_block.png"},
 	is_ground_content = false,
 	stack_max = 64,
@@ -177,8 +179,8 @@ minetest.register_craft({
 
 --Green Paladium
 minetest.register_node("pala_paladium:stone_with_gpaladium", {
-	description = ("Green Paladium Ore"),
-	_doc_items_longdesc = ("Green Paladium ore is rare and can be found in clusters near the bottom of the world."),
+	description = S("@1 Ore", S("Green Paladium")),
+	_doc_items_longdesc = S("@1 ore is rare and can be found in clusters near the bottom of the world.", S("Green Paladium")),
 	tiles = {"default_stone.png^pala_paladium_mineral_gpaladium.png"},
 	is_ground_content = true,
 	stack_max = 64,
@@ -189,16 +191,16 @@ minetest.register_node("pala_paladium:stone_with_gpaladium", {
 })
 
 minetest.register_craftitem("pala_paladium:gpaladium_ingot", {
-	description = ("Green Paladium Ingot"),
-	_doc_items_longdesc = ("Molten Green Paladium. It is used to craft armor, tools, and whatnot."),
+	description = S("@1 Ingot", S("Green Paladium")),
+	_doc_items_longdesc = S("Molten @1. It is used to craft armor, tools, and whatnot.", S("Green Paladium")),
 	inventory_image = "pala_paladium_gpaladium_ingot.png",
 	stack_max = 64,
 	groups = { craftitem=1 },
 })
 
 minetest.register_node("pala_paladium:gpaladiumblock", {
-	description = ("Block of Green Paladium"),
-	_doc_items_longdesc = ("A block of Green Paladium is mostly a shiny decorative block."),
+	description = S("Block of @1", S("Green Paladium")),
+	_doc_items_longdesc = S("A block of @1 is mostly a shiny decorative block.", S("Green Paladium")),
 	tiles = {"pala_paladium_gpaladium_block.png"},
 	is_ground_content = false,
 	stack_max = 64,
@@ -233,8 +235,8 @@ minetest.register_craft({
 
 --Findium
 minetest.register_node("pala_paladium:stone_with_findium", {
-	description = ("Findium Ore"),
-	_doc_items_longdesc = ("Findium ore is rare and can be found in clusters near the bottom of the world."),
+	description = S("@1 Ore", S("Findium")),
+	_doc_items_longdesc = S("@1 ore is rare and can be found in clusters near the bottom of the world.", S("Findium")),
 	tiles = {"pala_paladium_mineral_findium.png"},
 	is_ground_content = true,
 	stack_max = 64,
@@ -251,16 +253,16 @@ minetest.register_node("pala_paladium:stone_with_findium", {
 })
 
 minetest.register_craftitem("pala_paladium:findium_gem", {
-	description = ("Findium Gem"),
-	_doc_items_longdesc = ("Findium Gem. It is used to craft armor, tools, and whatnot."),
+	description = S("Findium Gem"),
+	_doc_items_longdesc = S("Findium Gem. It is used to craft armor, tools, and whatnot."),
 	inventory_image = "pala_paladium_findium_gem.png",
 	stack_max = 64,
 	groups = { craftitem=1 },
 })
 
 minetest.register_node("pala_paladium:findiumblock", {
-	description = ("Block of Findium"),
-	_doc_items_longdesc = ("A block of Findium is mostly a shiny decorative block."),
+	description = S("Block of @1", S("Findium")),
+	_doc_items_longdesc = S("A block of @1 is mostly a shiny decorative block.", S("Findium")),
 	tiles = {"pala_paladium_findiumblock.png"},
 	is_ground_content = false,
 	stack_max = 64,
