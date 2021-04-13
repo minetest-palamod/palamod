f4caec39939103b0e41b73a7cf10247b7db4d704
AFCMS <afcm.contact@gmail.com>
2021-04-13 23:58:22 +0200

    make teleport stick use pearls

--- pala_sticks/init.lua
+++ pala_sticks/init.lua
@@ -1,11 +1,29 @@
+local is_creative_enabled = minetest.is_creative_enabled
+local function take_pearl(player)
+	local inv = player:get_inventory()
+	local stack
+	for i=1, inv:get_size("main") do
+		local it = inv:get_stack("main", i)
+		if not it:is_empty() and it:get_name() == "mcl_throwing:ender_pearl" then
+			inv:set_stack("main", i, it:take_item())
+			return true
+		end
+	end
+	return false
+end
 minetest.register_tool("pala_sticks:teleport_stick", {
     description = "Teleport Stick",
     inventory_image = "default_stick.png",
 	on_use = function(itemstack, player, pointed_thing)
+		local name = player:get_player_name()
 		local pos = player:get_pos()
+		if not is_creative_enabled(name) then
+			if not take_pearl(player) then
+				return
+			end
+		end
 		mcl_throwing.throw("mcl_throwing:ender_pearl",
-			{x=pos.x, y=pos.y+1.5, z=pos.z}, player:get_look_dir(), 22,
-			player:get_player_name())
+			{x=pos.x, y=pos.y+1.5, z=pos.z}, player:get_look_dir(), 22, name)
 	end,
 })
 
