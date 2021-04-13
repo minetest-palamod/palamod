5251e762df0ba98dbd8cfd001f8ee133be586646
AFCMS <afcm.contact@gmail.com>
2021-04-13 23:50:22 +0200

    fix bad behaviour of teleport stick

--- pala_sticks/init.lua
+++ pala_sticks/init.lua
@@ -2,9 +2,10 @@ minetest.register_tool("pala_sticks:teleport_stick", {
     description = "Teleport Stick",
     inventory_image = "default_stick.png",
 	on_use = function(itemstack, player, pointed_thing)
+		local pos = player:get_pos()
 		mcl_throwing.throw("mcl_throwing:ender_pearl",
-			player:get_pos(), player:get_look_dir(), 22,
-			minetest.get_player_by_name(player))
+			{x=pos.x, y=pos.y+1.5, z=pos.z}, player:get_look_dir(), 22,
+			player:get_player_name())
 	end,
 })
 
@@ -19,5 +20,6 @@ minetest.register_tool("pala_sticks:heal_stick", {
 		else
 			player:set_hp(20)
 		end
+		--TODO: remove wear
 	end,
 })
