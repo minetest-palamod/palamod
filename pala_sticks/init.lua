a72b188b56dfffa5e32a871a5ac78fd20bc30207
AFCMS <afcm.contact@gmail.com>
2021-04-13 23:42:10 +0200

    fix bad behaviour with sticks

--- pala_sticks/init.lua
+++ pala_sticks/init.lua
@@ -6,12 +6,6 @@ minetest.register_tool("pala_sticks:teleport_stick", {
 			player:get_pos(), player:get_look_dir(), 22,
 			minetest.get_player_by_name(player))
 	end,
-    tool_capabilities = {
-		full_punch_interval = 20,
-        groupcaps= {
-            cracky={times={[1]=4.00, [2]=1.50, [3]=1.00}, uses=70, maxlevel=1}
-        }
-    }
 })
 
 minetest.register_tool("pala_sticks:heal_stick", {
@@ -26,10 +20,4 @@ minetest.register_tool("pala_sticks:heal_stick", {
 			player:set_hp(20)
 		end
 	end,
-	tool_capabilities = {
-		full_punch_interval = 20,
-        groupcaps= {
-            cracky={times={[1]=4.00, [2]=1.50, [3]=1.00}, uses=70, maxlevel=1}
-        }
-    }
 })
