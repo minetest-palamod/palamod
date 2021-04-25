e0f934ae7b7f413a8da701bf30cd0b7bc5a48fba
AFCMS <afcm.contact@gmail.com>
2021-04-25 23:47:04 +0200

    make pala_machines use the setting

--- pala_machines/init.lua
+++ pala_machines/init.lua
@@ -2,5 +2,8 @@ local modpath = minetest.get_modpath(minetest.get_current_modname())
 
 pala_machines = {}
 
-dofile(modpath.."/cobblebreaker.lua")
 dofile(modpath.."/potionstacker.lua")
+
+if minetest.settings:get_bool("palamod.experimental", false) then
+	dofile(modpath.."/cobblebreaker.lua")
+end
