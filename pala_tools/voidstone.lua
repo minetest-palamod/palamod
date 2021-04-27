85d4be6048bf54553755f87537b1bdc0c8bab320
AFCMS <afcm.contact@gmail.com>
2021-04-28 00:05:39 +0200

    translation improvement for voidstone

--- pala_tools/voidstone.lua
+++ pala_tools/voidstone.lua
@@ -1,6 +1,8 @@
 --Voidstone
 
---[[formspec_version[3]
+local S = minetest.get_translator(minetest.get_current_modname())
+
+--[[formspec_version[4]
 	size[12,7]
 	list[current_player;main;0.5,2;9,4;0]
 	label[0.5,0.5;Voidstone]
@@ -8,7 +10,7 @@
 	list[current_player;main;5.5,0.5;1,1;0]
 ]]
 -- pala_tools.void_form = table.concat({
-	-- "formspec_version[3]",
+	-- "formspec_version[4]",
 	-- "size[12,7]",
 	-- "real_coordinates[false]",
 	-- mcl_formspec.get_itemslot_bg(0.5,2,9,4),
@@ -21,11 +23,7 @@
 
 local trash = minetest.create_detached_inventory("voidstone_trash", {
 	allow_put = function(inv, listname, index, stack, player)
-		if minetest.is_creative_enabled(player:get_player_name()) then
-			return stack:get_count()
-		else
-			return 0
-		end
+		return stack:get_count()
 	end,
 	on_put = function(inv, listname, index, stack, player)
 		inv:set_stack(listname, index, "")
@@ -35,7 +33,6 @@ trash:set_size("main", 1)
 
 pala_tools.voidstone_form = table.concat({
 	"size[9.21,5.32]",
-	"real_coordinates[false]",
 	mcl_formspec.get_itemslot_bg(0.1,1.41,9,1),
 	mcl_formspec.get_itemslot_bg(0.1,2.49,9,1),
 	mcl_formspec.get_itemslot_bg(0.1,3.58,9,1),
@@ -44,15 +41,15 @@ pala_tools.voidstone_form = table.concat({
 	"list[current_player;main;0.1,2.49;9.0,1.0;18]",
 	"list[current_player;main;0.1,3.58;9.0,1.0;27]",
 	"list[current_player;main;0.1,4.66;9.0,1.0;0]",
-	"label[0.1,-0.15;Voidstone]",
+	"label[0.1,-0.15;"..S("Voidstone").."]",
 	mcl_formspec.get_itemslot_bg(4.1,0.11,1,1),
 	"list[detached:voidstone_trash;main;4.1,0.11;1.0,1.0;0]",
 	"listring[]",
 })
 
 minetest.register_craftitem("pala_tools:voidstone", {
-	description = "Voidstone",
-	_doc_items_longdesc = "Voidstone", --TODO: desc and translation
+	description = S("Voidstone"),
+	_doc_items_longdesc = S("Voidstone"), --TODO: desc and translation
 	inventory_image = "pala_tools_voidstone.png",
 	stack_max = 1,
 	groups = {},
