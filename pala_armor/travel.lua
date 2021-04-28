c2a3ceac93484239ca2765117d0470492f2bdd6f
AFCMS <afcm.contact@gmail.com>
2021-04-28 10:56:41 +0200

    slymy helmet working

---
+++ pala_armor/travel.lua
@@ -0,0 +1,27 @@
+local S = minetest.get_translator(minetest.get_current_modname())
+
+--Slimy Helmet
+minetest.register_tool("pala_armor:helmet_slimy", {
+	description = S("Slimy Helmet"),
+	_doc_items_longdesc = mcl_armor.longdesc..
+		" "..S("Negate fall damage"),
+	_doc_items_usagehelp = mcl_armor.usage,
+	inventory_image = "mcl_armor_inv_helmet_leather.png",
+	groups = {armor_head = 1, non_combat_head = 1, mcl_armor_points=1, mcl_armor_uses = 165},
+	sounds = {
+		_mcl_armor_equip = "slimenodes_step.1",
+		_mcl_armor_unequip = "slimenodes_step.1",
+	},
+	on_place = mcl_armor.equip_on_use,
+	on_secondary_use = mcl_armor.equip_on_use,
+	_mcl_armor_element = "head",
+	_mcl_armor_texture = "mcl_armor_helmet_leather.png"
+})
+
+--No fall damage if wearing slimy helmet
+mcl_damage.register_modifier(function(obj, damage, reason)
+    if reason.type == "fall" and obj:get_inventory():get_stack("armor", 2):get_name() == "pala_armor:helmet_slimy" then
+        return 0
+    end
+    return damage
+end, 199)
