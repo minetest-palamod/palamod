73633520ccf3a632f8033503e83ba3ede39c1f39
AFCMS <afcm.contact@gmail.com>
2021-04-30 00:27:32 +0200

    fix luacheck

--- pala_looting/chest_explorer.lua
+++ pala_looting/chest_explorer.lua
@@ -8,7 +8,7 @@ minetest.register_craftitem("pala_looting:chest_explorer", {
     stack_max = 1,
     on_place = function(itemstack, placer, pointed_thing)
         if placer:is_player() and placer:get_player_control().sneak and pointed_thing.type == "node" then
-            local nodename = minetest.get_node(pointed_thing.under)
+            --local nodename = minetest.get_node(pointed_thing.under)
             --if minetest.get_item_group(nodename, "container") == 0 then return end
             local meta = minetest.get_meta(pointed_thing.under)
             local inv = meta:get_inventory()
@@ -34,7 +34,8 @@ minetest.register_craftitem("pala_looting:chest_explorer", {
             --[[for i = 1, size/9 do
                 for j = 1, 9 do
                     local stack = inv:get_stack("main", (i*9)-9+j)
-                    form = form.."item_image["..(i+i*0.25)-1 ..","..(j+j*0.25)-1 ..";1,1;"..stack:get_name().." "..stack:get_count().."]"
+                    form = form..
+                    "item_image["..(i+i*0.25)-1 ..","..(j+j*0.25)-1 ..";1,1;"..stack:get_name().." "..stack:get_count().."]"
                     form = form.."tooltip["..(i+i*0.25)-1 ..",0.25;1,1;"..stack:get_description().."]"
                 end
             end]]
