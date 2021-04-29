231348f37489cc02b8d60b8e34c41d569e18bccf
AFCMS <afcm.contact@gmail.com>
2021-04-30 00:25:39 +0200

    semi working chest explorer

--- pala_looting/chest_explorer.lua
+++ pala_looting/chest_explorer.lua
@@ -8,11 +8,13 @@ minetest.register_craftitem("pala_looting:chest_explorer", {
     stack_max = 1,
     on_place = function(itemstack, placer, pointed_thing)
         if placer:is_player() and placer:get_player_control().sneak and pointed_thing.type == "node" then
-            --local nodename = minetest.get_node(pointed_thing.under)
+            local nodename = minetest.get_node(pointed_thing.under)
+            --if minetest.get_item_group(nodename, "container") == 0 then return end
             local meta = minetest.get_meta(pointed_thing.under)
             local inv = meta:get_inventory()
             local size = inv:get_size("main")
             --local width = inv:get_width("main")
+            --minetest.chat_send_all(width)
 
             --item_image[<X>,<Y>;<W>,<H>;<item name>]
             --local inv = minetest.get_inventory({ type="node", pos=pointed_thing.under })
@@ -20,14 +22,22 @@ minetest.register_craftitem("pala_looting:chest_explorer", {
             --local list = inv:get_list("main")
             local form = table.concat({
                 "formspec_version[4]",
-                "size[10,10]",
-                mcl_formspec.get_itemslot_bg(0.375,0.375, 9, 3),
+                "size[11.50,"..((size/9)+size/9*0.25)+0.25 .."]",
+                pala_paladium.get_itemslot_bg(0.25,0.25, 9, (size/9)),
             })
                 --mcl_formspec.get_itemslot_bg(0,4.5, width, size/width)
             for i = 1, size do
                 local stack = inv:get_stack("main", i)
-                form = form.."item_image["..-0.375+(1*i)..",0.375;1,1;"..stack:get_name().."]"
+                form = form.."item_image["..(i+i*0.25)-1 ..",0.25;1,1;"..stack:get_name().." "..stack:get_count().."]"
+                form = form.."tooltip["..(i+i*0.25)-1 ..",0.25;1,1;"..stack:get_description().."]"
             end
+            --[[for i = 1, size/9 do
+                for j = 1, 9 do
+                    local stack = inv:get_stack("main", (i*9)-9+j)
+                    form = form.."item_image["..(i+i*0.25)-1 ..","..(j+j*0.25)-1 ..";1,1;"..stack:get_name().." "..stack:get_count().."]"
+                    form = form.."tooltip["..(i+i*0.25)-1 ..",0.25;1,1;"..stack:get_description().."]"
+                end
+            end]]
             minetest.show_formspec(placer:get_player_name(), "chest_explorer", form)
         end
     end,
