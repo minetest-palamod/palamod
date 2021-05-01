0b0c093a48b5d1ccfb5a20f221d15c70fb0b27cd
AFCMS <afcm.contact@gmail.com>
2021-05-01 21:44:15 +0200

    chest explorer working

--- pala_looting/chest_explorer.lua
+++ pala_looting/chest_explorer.lua
@@ -1,5 +1,24 @@
 local S = minetest.get_translator(minetest.get_current_modname())
 
+local callbacks = {
+    allow_move = function() return 0 end,
+    allow_put = function() return 0 end,
+    allow_take = function() return 0 end,
+}
+local tmp_inventories = {}
+
+minetest.register_on_joinplayer(function(player)
+    local playername = player:get_player_name()
+    tmp_inventories[playername] = minetest.create_detached_inventory("pala_looting:chest_explorer_"..playername,
+        callbacks, playername)
+end)
+
+minetest.register_on_leaveplayer(function(player)
+    local playername = player:get_player_name()
+    minetest.remove_detached_inventory("pala_looting:chest_explorer_"..playername) 
+    tmp_inventories[playername] = nil
+end)
+
 --Chest Explorer
 minetest.register_craftitem("pala_looting:chest_explorer", {
     description = S("Chest Explorer"),
@@ -10,6 +29,7 @@ minetest.register_craftitem("pala_looting:chest_explorer", {
         if placer:is_player() and placer:get_player_control().sneak and pointed_thing.type == "node" then
             --local nodename = minetest.get_node(pointed_thing.under)
             --if minetest.get_item_group(nodename, "container") == 0 then return end
+            local playername = placer:get_player_name()
             local meta = minetest.get_meta(pointed_thing.under)
             local inv = meta:get_inventory()
             local size = inv:get_size("main")
@@ -17,20 +37,28 @@ minetest.register_craftitem("pala_looting:chest_explorer", {
             --minetest.chat_send_all(width)
 
             --item_image[<X>,<Y>;<W>,<H>;<item name>]
-            --local inv = minetest.get_inventory({ type="node", pos=pointed_thing.under })
             --local size = inv:get_size("main")
-            --local list = inv:get_list("main")
+            local list = inv:get_list("main")
+            local tmp_inv = tmp_inventories[playername]
+            if not tmp_inv then return end --TODO: error
+
+            tmp_inv:set_size("main", size)
+            tmp_inv:set_list("main", list)
+
+            local width = size/9
+
             local form = table.concat({
                 "formspec_version[4]",
-                "size[11.50,"..((size/9)+size/9*0.25)+0.25 .."]",
-                pala_paladium.get_itemslot_bg(0.25,0.25, 9, (size/9)),
+                "size[11.50,"..(width+width*0.25)+0.25 .."]",
+                pala_paladium.get_itemslot_bg(0.25,0.25, 9, width),
+                "list[detached:pala_looting:chest_explorer_"..playername..";main;0.25,0.25;9,"..width..";]"
             })
                 --mcl_formspec.get_itemslot_bg(0,4.5, width, size/width)
-            for i = 1, size do
+            --[[for i = 1, size do
                 local stack = inv:get_stack("main", i)
                 form = form.."item_image["..(i+i*0.25)-1 ..",0.25;1,1;"..stack:get_name().." "..stack:get_count().."]"
                 form = form.."tooltip["..(i+i*0.25)-1 ..",0.25;1,1;"..stack:get_description().."]"
-            end
+            end]]
             --[[for i = 1, size/9 do
                 for j = 1, 9 do
                     local stack = inv:get_stack("main", (i*9)-9+j)
@@ -39,7 +67,7 @@ minetest.register_craftitem("pala_looting:chest_explorer", {
                     form = form.."tooltip["..(i+i*0.25)-1 ..",0.25;1,1;"..stack:get_description().."]"
                 end
             end]]
-            minetest.show_formspec(placer:get_player_name(), "chest_explorer", form)
+            minetest.show_formspec(placer:get_player_name(), "pala_looting:chest_explorer", form)
         end
     end,
 })
