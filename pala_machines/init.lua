c2b4aac26d6e7c3d1dc28778b7c467b44224d561
AFCMS <afcm.contact@gmail.com>
2021-05-02 18:34:12 +0200

    refine potionstacker

--- pala_machines/init.lua
+++ pala_machines/init.lua
@@ -2,8 +2,7 @@ local modpath = minetest.get_modpath(minetest.get_current_modname())
 
 pala_machines = {}
 
-dofile(modpath.."/potionstacker.lua")
-
 if minetest.settings:get_bool("palamod.experimental", false) then
 	dofile(modpath.."/cobblebreaker.lua")
+	dofile(modpath.."/potionstacker.lua")
 end
