9a2d9eb36505de9248123be5b0bd4c28cf084aa4
AFCMS <afcm.contact@gmail.com>
2021-05-10 23:46:15 +0200

    cleanup

--- pala_grade/commands.lua
+++ pala_grade/commands.lua
@@ -1,6 +1,6 @@
 local S = minetest.get_translator(minetest.get_current_modname())
 local C = minetest.colorize
-local color_escape = minetest.get_color_escape_sequence
+local F = minetest.get_color_escape_sequence
 
 minetest.register_chatcommand("feed", {
 	params = "",
@@ -16,7 +16,7 @@ minetest.register_chatcommand("feed", {
 			return true, C(mcl_colors.GREEN, S("You have been feed!"))
         else
             return false, C(mcl_colors.RED, S("You must have the [@1] grade to run this command.",
-				pala_grade.grades.hero.desc..color_escape(mcl_colors.RED)))
+				pala_grade.grades.hero.desc..F(mcl_colors.RED)))
         end
 	end,
 })
@@ -31,30 +31,32 @@ minetest.register_chatcommand("craft", {
 	end,
 })
 
-minetest.register_chatcommand("furnace", {
-	params = "",
-	description = "Allows you to cook an item without a furnace",
-	privs = {interact = true},
-	func = function(name, param)
-		local player = minetest.get_player_by_name(name)
-		if not player then
-			return false, "Player not found"
-		end
-		local item = player:get_wielded_item()
-		local output = minetest.get_craft_result({
-            method = "cooking",
-            width = 1,
-            items = {item}
-        })
-		if output and output.item and not output.item:is_empty() then
-			minetest.chat_send_player(name, tostring(output))
-			--local outputname = output.item:get_name(),
-			--minetest.chat_send_player(name, tostring(outputname))
-			minetest.Chat_send_player(name, "WIP")
-			--player:set_wielded_item(outputname)
-            --local count = output.item:to_table().count
-		end
-		--if output == item
-		mcl_hunger.set_hunger(player, 20)
-	end,
-})
+if minetest.settings:get_bool("palamod.experimental", false) then
+	minetest.register_chatcommand("furnace", {
+		params = "",
+		description = "Allows you to cook an item without a furnace",
+		privs = {interact = true},
+		func = function(name, param)
+			local player = minetest.get_player_by_name(name)
+			if not player then
+				return false, "Player not found"
+			end
+			local item = player:get_wielded_item()
+			local output = minetest.get_craft_result({
+				method = "cooking",
+				width = 1,
+				items = {item}
+			})
+			if output and output.item and not output.item:is_empty() then
+				minetest.chat_send_player(name, tostring(output))
+				--local outputname = output.item:get_name(),
+				--minetest.chat_send_player(name, tostring(outputname))
+				minetest.chat_send_player(name, "WIP")
+				--player:set_wielded_item(outputname)
+				--local count = output.item:to_table().count
+			end
+			--if output == item
+			mcl_hunger.set_hunger(player, 20)
+		end,
+	})
+end
