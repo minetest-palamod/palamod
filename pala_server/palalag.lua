dd59f87bdedefd660b172a72aea1695053f32473
AFCMS <afcm.contact@gmail.com>
2021-05-16 00:18:58 +0200

    [pala_server] palalag logging and better chat command

--- pala_server/palalag.lua
+++ pala_server/palalag.lua
@@ -2,27 +2,32 @@ local S = minetest.get_translator(minetest.get_current_modname())
 local C = minetest.colorize
 
 local math = math
+--local string = string
 
 if minetest.settings:get_bool("pala_server.clear_lag", true) then
     local msg_base = C(mcl_colors.GOLD, "[PalaLag] ")
     local msg1 = msg_base..C(mcl_colors.RED, S("Entities will be deleted in 30s!"))
     local msg2 = msg_base..C(mcl_colors.RED, S("Entities have been deleted!"))
     local CLEAR_INTERVAL = tonumber(minetest.settings:get("pala_server.clear_lag_interval") or 300)
-    --local CLEAR_INTERVAL = 40
+    --CLEAR_INTERVAL = 31
     local time = 0
 
     minetest.register_globalstep(function(dtime)
         time = time + dtime;
         if time >= CLEAR_INTERVAL then
             minetest.chat_send_all(msg1)
+            minetest.log("info", "[pala_server] Palalag will start in 30s")
             minetest.after(30, function()
+                minetest.log("info", "[pala_server] Palalag starting...")
+                local count = 0
                 for id, val in pairs(minetest.luaentities) do
                     local def = minetest.registered_entities[minetest.luaentities[id].name]
                     if def._cmi_is_mob or val.name == "__builtin:item" then
                         minetest.luaentities[id].object:remove()
-                        --count = count + 1
+                        count = count + 1
                     end
                 end
+                minetest.log("info", "[pala_server] Palalag removed "..count.." entities")
                 minetest.chat_send_all(msg2)
             end)
             time = 0
@@ -32,7 +37,7 @@ if minetest.settings:get_bool("pala_server.clear_lag", true) then
         params = "",
         description = S("Allow you to show next clearlag"),
         func = function(name, param)
-            return true, S("Next clearlag in @1s", math.round(CLEAR_INTERVAL-time))
+            return true, C(mcl_colors.GOLD, S("Next clearlag in @1s", math.floor(CLEAR_INTERVAL-time+30)))
         end,
     })
 end
