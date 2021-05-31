88af9e1cfa338dc17dc175eb8b5239c2ae126581
AFCMS <afcm.contact@gmail.com>
2021-05-31 21:33:29 +0200

    [pala_looting] fix indentation

--- pala_looting/onlinedetector.lua
+++ pala_looting/onlinedetector.lua
@@ -39,10 +39,12 @@ minetest.register_node("pala_looting:online_detector_off", {
 	_doc_items_longdesc = S("Allows you to know if a player is connected."),
 	tiles = {"pala_looting_online_detector_off.png"},
 	groups = {pickaxey=2, building_block=1, material_stone=1, pala_online_detector=1},
-	mesecons = {receptor = {
-		state = mesecon.state.off,
-		rules = mesecon.rules.alldirs,
-	}},
+	mesecons = {
+		receptor = {
+			state = mesecon.state.off,
+			rules = mesecon.rules.alldirs,
+		},
+	},
 	on_construct = function(pos)
 		local meta = minetest.get_meta(pos)
 		meta:set_string("formspec", get_detector_form(meta:get_string("name")))
@@ -63,10 +65,12 @@ minetest.register_node("pala_looting:online_detector_on", {
 	tiles = {"pala_looting_online_detector_on.png"},
 	groups = {pickaxey=2, building_block=1, material_stone=1, pala_online_detector=1, not_in_creative_inventory=1},
 	drop = "pala_looting:online_detector_off",
-	mesecons = {receptor = {
-		state = mesecon.state.on,
-		rules = mesecon.rules.alldirs,
-	}},
+	mesecons = {
+		receptor = {
+			state = mesecon.state.on,
+			rules = mesecon.rules.alldirs,
+		},
+	},
 	on_construct = function(pos)
 		local meta = minetest.get_meta(pos)
 		meta:set_string("formspec", get_detector_form(meta:get_string("name")))
