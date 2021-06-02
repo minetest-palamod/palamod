af73b71a4194cb7373b5c2c4e89e7d94b34f1783
AFCMS <afcm.contact@gmail.com>
2021-06-03 00:35:15 +0200

    [pala_armor] add weighted boots textures

--- pala_armor/extra.lua
+++ pala_armor/extra.lua
@@ -11,7 +11,7 @@ if minetest.settings:get_bool("palamod.experimental", false) then
         _doc_items_longdesc = mcl_armor.longdesc..
             " "..S("Allows you to descend faster to the bottom of the water and stay there"),
         _doc_items_usagehelp = mcl_armor.usage,
-        inventory_image = "mcl_armor_inv_boots_leather.png",
+        inventory_image = "pala_armor_inv_boots_weighted.png",
         groups = {armor_feet = 1, non_combat_torso = 1, mcl_armor_points=0.5, mcl_armor_uses = 66},
         sounds = {
             _mcl_armor_equip = "mcl_armor_equip_leather",
@@ -20,6 +20,6 @@ if minetest.settings:get_bool("palamod.experimental", false) then
         on_place = mcl_armor.equip_on_use,
         on_secondary_use = mcl_armor.equip_on_use,
         _mcl_armor_element = "feet",
-        _mcl_armor_texture = "mcl_armor_boots_leather.png"
+        _mcl_armor_texture = "pala_armor_boots_weighted.png",
     })
 end
