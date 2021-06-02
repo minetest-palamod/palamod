c5a64d733afed391327457899f74d78134421902
AFCMS <afcm.contact@gmail.com>
2021-06-03 01:01:35 +0200

    [pala_mapgen] refactor the code to be more efficient

--- pala_mapgen/init.lua
+++ pala_mapgen/init.lua
@@ -4,7 +4,12 @@ pala_mapgen = {}
 
 local modpath = minetest.get_modpath(minetest.get_current_modname())
 
-dofile(modpath.."/ores.lua")
+dofile(modpath.."/vars.lua")
+
+if minetest.settings:get_bool("pala_mapgen.generate_ores", true) then
+	dofile(modpath.."/ores.lua")
+end
+
 dofile(modpath.."/biome_volcano.lua")
 
 minetest.log("action", "[pala_mapgen] loaded succesfully")
---
+++ pala_mapgen/vars.lua
@@ -0,0 +1,4 @@
+pala_mapgen.AMETHYST_LAYER = 25
+pala_mapgen.TITANIUM_LAYER = 25
+pala_mapgen.PALADIUM_LAYER = 16
+pala_mapgen.FINDIUM_LAYER = 20
