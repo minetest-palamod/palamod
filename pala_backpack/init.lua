fec43e75c500bb5c0267ee46210c3b1decd13063
AFCMS <afcm.contact@gmail.com>
2021-06-28 20:25:44 +0200

    [pala_backpack] fix warning

--- pala_backpack/init.lua
+++ pala_backpack/init.lua
@@ -77,7 +77,7 @@ local backpack_widths = {
 local form = table.concat({
 	"formspec_version[4]",
 	"size[12,18]",
-	"list[current_player;backpack;0.5,1;9,9;]", 
+	"list[current_player;backpack;0.5,1;9,9;]",
 	"list[current_player;main;0.5,13;9,4;]",
 	"listring[]",
 	"label[0.5,0.5;Backpack]",
