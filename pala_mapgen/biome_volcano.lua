36f58dc7f3c548b5f5b9fe3e0a05309fc0f0b154
AFCMS <afcm.contact@gmail.com>
2021-09-22 23:35:37 +0200

    [pala_mapgen] mark volcano biome as experimental

--- pala_mapgen/biome_volcano.lua
+++ pala_mapgen/biome_volcano.lua
@@ -1,19 +1,23 @@
-minetest.register_biome({
-        name = "volcano",
-        node_dust = "mcl_core:obsidian",
-        node_top = "mcl_core:obsidian",
-        depth_top = 1,
-        node_filler = "mcl_core:obsidian",
-        depth_filler = 7,
-        node_stone = "mcl_core:obsidian",
-        node_water_top = "mcl_core:water_source",
-        depth_water_top = 10,
-        node_water = "",
-        node_river_water = "mcl_core:lava_source",
-        node_riverbed = "mcl_core:stone",
-        depth_riverbed = 3,
-        y_min = 5,
-        y_max = 75,
-        heat_point = 40,
-        humidity_point = 10,
-})
+--A volcano biome, mostly composed by obsidian
+
+if minetest.settings:get_bool("palamod.experimental", false) then
+	minetest.register_biome({
+		name = "volcano",
+		node_dust = "mcl_core:obsidian",
+		node_top = "mcl_core:obsidian",
+		depth_top = 1,
+		node_filler = "mcl_core:obsidian",
+		depth_filler = 7,
+		node_stone = "mcl_core:obsidian",
+		node_water_top = "mcl_core:water_source",
+		depth_water_top = 10,
+		node_water = "",
+		node_river_water = "mcl_core:lava_source",
+		node_riverbed = "mcl_core:stone",
+		depth_riverbed = 3,
+		y_min = 5,
+		y_max = 75,
+		heat_point = 40,
+		humidity_point = 10,
+	})
+end
