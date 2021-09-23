b1814c7cc21722d5d27f7e1fd258933896d287ac
AFCMS <afcm.contact@gmail.com>
2021-09-23 21:30:35 +0200

    [pala_server] different join message for admins

--- pala_server/msg.lua
+++ pala_server/msg.lua
@@ -15,3 +15,14 @@ else
     pala_server.messages.enter_nopvp = C(mcl_colors.DARK_GREEN, S("Welcome to @1", C(mcl_colors.GOLD , S("Spawn"))))
     pala_server.messages.enter_pvp = C(mcl_colors.DARK_GREEN, S("You just entered @1", C(mcl_colors.RED , S("Warzone"))))
 end
+
+--https://youtu.be/7D1fcB6Addg?t=84
+function minetest.send_join_message(player_name)
+	if not core.is_singleplayer() then
+		if minetest.get_player_privs(player_name).server == true then
+			minetest.chat_send_all(C(mcl_colors.RED, S("[Admin] @1 as joined the server")))
+		else
+			minetest.chat_send_all("*** " .. player_name .. " joined the game.")
+		end
+	end
+end
