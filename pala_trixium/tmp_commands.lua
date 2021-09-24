3be34357ab612490ce20ee9abf87d3d74f21456d
AFCMS <afcm.contact@gmail.com>
2021-09-24 23:59:06 +0200

    [pala_trixium] add command to show rankings

--- pala_trixium/tmp_commands.lua
+++ pala_trixium/tmp_commands.lua
@@ -6,9 +6,15 @@ minetest.register_chatcommand("add_trixium", {
 		if not player then
 			return false, "Player isn't online"
 		end
-		local item_count = player:get_wielded_item():get_count()
-		pala_trixium.add_amount(name, item_count)
-		player:set_wielded_item(ItemStack())
+		local item = player:get_wielded_item()
+
+		local group = minetest.get_item_group(item:get_name(), "trixium_amount")
+		if group > 0 then
+			pala_trixium.add_amount(name, group * item:get_count())
+			player:set_wielded_item(ItemStack())
+			return true, "Done."
+		end
+		return false, "Not right minerals"
 	end,
 })
 
@@ -29,5 +35,17 @@ minetest.register_chatcommand("get_trixium", {
 			return false, "Player isn't online"
 		end
 		return true, pala_trixium.get_amount(name)
+	end
+})
+
+--TODO: add inventory tab
+minetest.register_chatcommand("ranking_trixium", {
+	func = function(name)
+		local top_rank = pala_trixium.get_top_ranking()
+		local out = ""
+		for num,entry in pairs(top_rank) do
+			out = out..num..": "..entry.name.." - "..entry.amount.."\n"
+		end
+		return true, out
 	end
 })
