829a612b5cf097e8bb603a130aa002833fd5a8ad
AFCMS <afcm.contact@gmail.com>
2022-01-08 23:42:01 +0100

    [pala_tools] remove unused line

--- pala_tools/infernalknocker.lua
+++ pala_tools/infernalknocker.lua
@@ -16,8 +16,6 @@ minetest.register_tool("pala_tools:infernal_knocker", {
 		full_punch_interval = 0.625,
 		max_drop_level=1,
 		groupcaps={
-			--swordy_dig = {times=5, uses=60, maxlevel=0},
-			--swordy_cobweb_dig = {times=1, uses=60, maxlevel=0},
 			swordy = { speed = 5, level = 1, uses = 60 },
 			swordy_cobweb = { speed = 1, level = 1, uses = 60 }
 		},
