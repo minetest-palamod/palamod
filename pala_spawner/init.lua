3f062923266bd8828ed3bc56ca2030ae22f254de
AFCMS <afcm.contact@gmail.com>
2022-07-09 01:08:36 +0200

    [pala_spawner] WIP

--- pala_spawner/init.lua
+++ pala_spawner/init.lua
@@ -1,29 +1,54 @@
 minetest.log("action", "[pala_spawner] loading...")
 
-if minetest.settings:get_bool("palamod.experimental", false) then
-	minetest.register_node("pala_spawner:spawner_brocken", {
-		tiles = {"mob_spawner.png"},
-		drawtype = "glasslike",
-		paramtype = "light",
-		walkable = true,
-		description = ("Brocken Mob Spawner"),
-		_tt_help = ("Brocken Mob Spawner"),
-		_doc_items_longdesc = ([[
-			A mob spawner regularily causes mobs to appear around it while a player is nearby.
-			Some mob spawners are disabled while in light.
-			]]),
-		_doc_items_usagehelp = ([[
-			If you have a spawn egg, you can use it to change the mob to spawn.
-			Just place the item on the mob spawner.
-			Player-set mob spawners always spawn mobs regardless of the light level.
-			]]),
-		groups = {pickaxey=1, material_stone=1, deco_block=1},
-		is_ground_content = false,
-		drop = "",
-		sounds = mcl_sounds.node_sound_metal_defaults(),
-		_mcl_blast_resistance = 5,
-		_mcl_hardness = 5,
-	})
-end
+local S = minetest.get_translator("pala_spawner")
+
+mcl_autogroup.register_diggroup("hammer_spawner")
+
+
+-- Cavern Hammer
+minetest.register_tool("pala_spawner:cavern_hammer", {
+	description = S("Cavern Hammer"),
+	_doc_items_longdesc = S("Used for modded spawners"),
+	inventory_image = "pala_tools_infernalknocker.png",
+	wield_scale = mcl_vars.tool_wield_scale,
+	groups = {tool = 1, dig_speed_class = 2, enchantability = 12},
+	sound = { breaks = "default_tool_breaks" },
+	tool_capabilities = {
+		full_punch_interval = 1,
+		max_drop_level = 5,
+		damage_groups = {fleshy=4},
+		punch_attack_uses = 4999,
+	},
+	_mcl_toollike_wield = true,
+	_mcl_diggroups = {
+		hammer_spawner = { speed = 9, level = 5, uses = 4999 }
+	},
+})
+
+minetest.override_item("mcl_mobspawners:spawner", {
+	groups = {pickaxey = 1, material_stone = 1, deco_block = 1, hammer_spawner = 1},
+	after_dig_node = function(pos, oldnode, oldmetadata, digger)
+		if digger:get_wielded_item():get_name() == "pala_spawner:cavern_hammer" then
+			minetest.chat_send_all("Stuff")
+			local mob = oldmetadata.fields.Mob
+			minetest.chat_send_all(mob)
+		end
+	end,
+})
+
+
+minetest.register_node("pala_spawner:spawner_broken", {
+	tiles = {"mob_spawner.png"},
+	drawtype = "glasslike",
+	paramtype = "light",
+	walkable = true,
+	description = ("Brocken Mob Spawner"),
+	_tt_help = S("Brocken Mob Spawner"),
+	groups = {pickaxey = 1, material_stone = 1, deco_block = 1},
+	is_ground_content = false,
+	sounds = mcl_sounds.node_sound_metal_defaults(),
+	_mcl_blast_resistance = 5,
+	_mcl_hardness = 5,
+})
 
 minetest.log("action", "[pala_server] loaded succesfully")
