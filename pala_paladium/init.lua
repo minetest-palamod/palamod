a8e9ae8419ced99b77d70718bbcd77a46848beee
Bapt-tech <137829833+Bapt-Tech@users.noreply.github.com>
2024-12-26 12:23:32 +0100

    replaced pala_paladium.code by pala_paladium_code

--- pala_paladium/init.lua
+++ pala_paladium/init.lua
@@ -1,18 +1,16 @@
 minetest.log("action", "[pala_paladium] loading...")
 
-local S = minetest.get_translator(minetest.get_current_modname())
-local C = minetest.colorize
+S = minetest.get_translator(minetest.get_current_modname())
+C = minetest.colorize
 
-pala_paladium = {}
+local pala_paladium_code = minetest.get_modpath("pala_paladium") .. "/ores"
 
-local pala_paladium.code = minetest.get_modpath("pala_paladium") .. "/ores"
-
-dofile(pala_paladium.code .. "/paladium/paladium.lua")
-dofile(pala_paladium.code .. "/amethyst/amethyst.lua")
-dofile(pala_paladium.code .. "/endium/endium.lua")
-dofile(pala_paladium.code .. "/findium/findium.lua")
-dofile(pala_paladium.code .. "/green_paladium/green_paladium.lua")
-dofile(pala_paladium.code .. "/titanium/titanium.lua")
-dofile(pala_paladium.code .. "/trixium/trixium.lua")
+dofile(pala_paladium_code .. "/paladium/paladium.lua")
+dofile(pala_paladium_code .. "/amethyst/amethyst.lua")
+dofile(pala_paladium_code .. "/endium/endium.lua")
+dofile(pala_paladium_code .. "/findium/findium.lua")
+dofile(pala_paladium_code .. "/green_paladium/green_paladium.lua")
+dofile(pala_paladium_code .. "/titanium/titanium.lua")
+dofile(pala_paladium_code .. "/trixium/trixium.lua")
 
 minetest.log("action", "[pala_paladium] loaded succesfully")
